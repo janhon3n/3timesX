198d4e5aab455a41d3296474bb7f67effe485692
root <root@JanhoWeb2.0>
2016-06-03 02:49:39 +0300

    Modified the form.php file a lot.
    The interface is a bit different. The form now send the data to the show.php function via GET.

--- form.php
+++ form.php
@@ -1,16 +1,18 @@
 <?php
 require 'head.php';
-
 ?>
+
 <body>
 <h1>3 times X</h1>
-<form>
-<input id="xCount" type="number" min="1" max="5" value="3">
-<div id="xInputs">
+<form method="get" action="show.php">
+Number of judges: <input name="xCount" id="xCount" min="1" max="9" type="number" value="3">
+<div id="xInputs" xCount="0">
 </div>
+
+<input type="submit" class="submitLink" new_window="0" value="Open in this window">
+<input type="submit" class="submitLink" new_window="1" value="Open in a new window">
+
 </form>
 
-<div class="submitLink" new_window="0">Open in this window</div>
-<div class="submitLink" new_window="1">Open in a new window</div>
-
+<script src="js/form.js"></script>
 </body>
