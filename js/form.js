01b46f8c47ded991cb3f4f677c790640cb13238d
root <root@JanhoWeb2.0>
2016-06-03 02:52:30 +0300

    Created a lot more of the functionality in javascript. Added the sound effect which will play on x activation

--- js/form.js
+++ js/form.js
@@ -1,31 +1,52 @@
 $(document).ready(function(){
 	
-	function updateForm(){
-		var xCount = $("form input#xCount").val();
-		var html = '';
-		for(var i = 0; i < xCount; i++){
-			html += createXInput();
-		}
-		$("form div#xInputs").html(html);
-	}
-
-	function createXInput(){
-			var html = '<div class="xInput"><input type="text" placeholder="Enter a name..."></div>';
+	function createXInput(number){
+			var html = '<div xNumber="'+number+'" class="xInput">'+number+'<br><input name="xInput[]" type="text" placeholder="Enter a name..."></div>';
 			return html;
 	}
+
+	function addXInput(number){
+		$("form #xInputs").append(createXInput(number));
+	}
+	function removeXInput(){
+		$("form #xInputs .xInput").last().remove();
+	}
+	
+	function updateXCount(){
+		var xCount = $("form input#xCount").val();
+		var currentXCount = parseInt($("form #xInputs").attr("xCount"));
+		if(xCount > currentXCount){
+			for(var i = currentXCount + 1; i <= xCount; i++){
+				addXInput(i);
+			}
+		} else if(xCount < currentXCount){
+			for(var i = currentXCount; i > xCount; i--){
+				removeXInput();
+			}
+		}
+		$("form #xInputs").attr("xCount", xCount);
+	}
+	
+	
+	//setup the xInputs changing functionality
+	
 	
 	$("form input#xCount").change(function(){
-		updateForm();
+		updateXCount();
 	});
-	
+	updateXCount();
+
+
 	$("div.submitLink").click(function(){
-			if($(this).attr("new_window") == 1){
-				console.log("new");
+		if($(this).attr("new_window") == 1){
+
 			} else {
-				console.log("old");
+				var html = "";
+				$("form .xInput").each(function(index){
+					var name = $(this).find("input").val();
+					html += '<div class="xShow" xNumber="'+index+'"><svg><line x1="5%" y1="5%" x2="95%" y2="95%"></svg>'+name+'</div>';
+				});
+				$("body").html(html);
 			}
 	});
-
-	updateForm();
-		
 });
---
+++ js/show.js
@@ -0,0 +1,66 @@
+$(document).ready(function(){
+	var xCount = parseInt($("#xShows").attr("xcount"));
+
+	function fixVerticalAlign(){
+		var xShowsHeight = parseInt($("#xShows").height());
+		var windowHeight = parseInt($(document).height());
+		console.log(xShowsHeight);
+		console.log(windowHeight);
+		$("#xShows").css("margin-top", (windowHeight / 2) - (xShowsHeight / 2));
+	}
+	
+	$(window).resize(function(){
+		fixVerticalAlign();
+	});
+	
+	fixVerticalAlign();
+	
+	function toggleActivateX(index){
+		var activeStatus = $("#xShows .xShow:nth-child("+(index)+")").attr("activated");
+		console.log(activeStatus);
+		if(activeStatus == "activated"){
+			deactivateX(index);
+		} else {
+			activateX(index);
+		}
+	}
+	
+	function activateX(index){
+		$("#xShows .xShow:nth-child("+(index)+")").attr("activated", "activated");
+		playSoundFx();
+	}
+	function deactivateX(index){
+		$("#xShows .xShow:nth-child("+(index)+")").attr("activated", "no");
+	}
+	
+	function deactivateAll(){
+		for(var i = 1; i <= xCount; i++){
+				deactivateX(i);
+		}
+	}
+	
+	var soundfx = [];
+	soundfx.push(new Audio("soundfx.mp3"));
+	soundfx.push(new Audio("soundfx.mp3"));
+	soundfx.push(new Audio("soundfx.mp3"));
+	var nextSoundfx = 0;
+	function playSoundFx(){
+		soundfx[nextSoundfx].pause();
+		soundfx[nextSoundfx].currentTime = 1.1;
+		soundfx[nextSoundfx].play();
+		
+		nextSoundfx += 1;
+		if(nextSoundfx >= soundfx.length){
+			nextSoundfx = 0;
+		}
+	}
+	
+	var keyboardCodesStart = 49;
+	$(document).keypress(function(e){
+		if(e.which >= keyboardCodesStart && e.which < keyboardCodesStart+xCount){
+			toggleActivateX(e.which - keyboardCodesStart + 1);
+		} else if(e.keyCode === 0 || e.keyCode === 32){
+			deactivateAll();
+		}
+	});
+});
